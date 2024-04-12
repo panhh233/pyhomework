57c3265e55cb8d3a268ed846613fc75a0d581130
panhh <panhh24944@gmail.com>
2024-04-12 17:52:59 +0800

    add three classes

--- append_1.py
+++ append_1.py
@@ -1,2 +1,31 @@
-def fib(a):
-    a == 2
+class MyClass(object):
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+    
+    def show(self):
+        print(self.a, self.b, self.c)
+
+    def add(self):
+        print(self.a + self.b + self.c)
+
+class MySecondClass(MyClass):
+    def __init__(self, a, b, c, d):
+        super(MySecondClass, self).__init__(a, b, c)
+        self.d = d
+    
+    def add(self):
+        print(self.a + self.b + self.c + self.d)
+
+    def show(self):
+        print(self.a, self.b, self.c, self.d)
+
+class MyThirdClass(MySecondClass):
+    def max(self):
+        print(max(self.a, self.b, self.c, self.d))
+
+obj1 = MyThirdClass(1, 2, 3, 4)
+obj1.show()
+obj1.add()
+obj1.max()
