23125bb952cc1b78c4435327ec897f17e71b65e6
panhh <panhh24944@gmail.com>
2024-04-29 10:25:38 +0800

    change 7_3.py

--- chaoxing/Experiment 7 Homework/7_3.py
+++ chaoxing/Experiment 7 Homework/7_3.py
@@ -5,7 +5,7 @@ k = int(input())
 people = [i for i in range(1, m + 1)]
 count = 0
 index = 0
-while count <= n:
+while count < n:
     index = (index + k - 1) % len(people)
     print('{}号下船了'.format(people[index]))
     people.remove(people[index])
